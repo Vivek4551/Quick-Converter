92c2a48cb2d9a001f740dff0736830ca1fc23aae
Vivek4551 <workwithvivek70@gmail.com>
2023-12-02 00:19:54 +0530

    resolved cors

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,4 +2,4 @@ const apiKey_1 = "157bd74521b12d0172d26acc21c3651b";
 const apiKey_2 = "258866f4986ebda66455925abdcfb439";
 
 export const currencyApiLink =
-  `http://apilayer.net/api/live?access_key=${apiKey_1 || apiKey_2}&currencies=USD,AED,GBP,CAD,SGD,EUR,JPY,PKR,ZAR,INR,ALL&source=INR&format=1`;
+  `https://corsproxy.io/?http://apilayer.net/api/live?access_key=${apiKey_1 || apiKey_2}&currencies=USD,AED,GBP,CAD,SGD,EUR,JPY,PKR,ZAR,INR,ALL&source=INR&format=1`;
